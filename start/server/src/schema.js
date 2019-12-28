1681652d7b2a0d9f27efd93d1c55b7b2ff6228a8
shgtkshruch <shgtk.shruch@gmail.com>
2019-12-28 12:15:09 +0900

    Add initial query

--- start/server/src/schema.js
+++ start/server/src/schema.js
@@ -1,7 +1,12 @@
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-  # Your schema will go here
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    # Queries for the current user
+    me: User
+  }
 `;
 
 module.exports = typeDefs;
