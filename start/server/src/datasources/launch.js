4d8c6be8cf82d5b12a267e1acf58cc9ccf6fee4c
shgtkshruch <shgtk.shruch@gmail.com>
2019-12-28 15:37:25 +0900

    Add launchReducer method

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -5,6 +5,34 @@ class LaunchAPI extends RESTDataSource {
     super();
     this.baseURL = 'https://api.spacexdata.com/v2/';
   }
+
+  // leaving this inside the class to make the class easier to test
+  launchReducer(launch) {
+    return {
+      id: launch.flight_number || 0,
+      cursor: `${launch.launch_date_unix}`,
+      site: launch.launch_site && launch.launch_site.site_name,
+      mission: {
+        name: launch.mission_name,
+        missionPatchSmall: launch.links.mission_patch_small,
+        missionPatchLarge: launch.links.mission_patch,
+      },
+      rocket: {
+        id: launch.rocket.rocket_id,
+        name: launch.rocket.rocket_name,
+        type: launch.rocket.rocket_type,
+      },
+    };
+  }
+
+  async getAllLaunches() {
+    const response = await this.get('launches');
+
+    // transform the raw launches to a more friendly
+    return Array.isArray(response)
+      ? response.map(launch => this.launchReducer(launch))
+      : [];
+  }
 }
 
 module.exports = LaunchAPI;
